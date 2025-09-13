9d57f0226a0e443e0d142f0a4f8d33a37be9707a
Param <parambansal97@gmail.com>
2025-09-14 05:23:12 +0530

    backend url

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(urlencoded({extended:true}));
 
 const corsOptions = {
   // https://connectapp-k6fs.onrender.com/
-    origin: process.env.URL, //  frontend
+    origin: 'https://connectapp-k6fs.onrender.com', //  frontend
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:process.env.URL,
+        origin:'https://connectapp-k6fs.onrender.com',
         methods:['GET','POST']
     }
 })
