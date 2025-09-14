f2379002c9f4251d1afbbfe0f5c364303f7b1bda
Param <parambansal97@gmail.com>
2025-09-14 07:19:28 +0530

    frontend path

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.use("/api/v1/message", (req, res, next) => {
 // Serve frontend build
 app.use(express.static(path.join(__dirname, "frontend", "dist")));
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 });
 
 // Start server after DB connection
